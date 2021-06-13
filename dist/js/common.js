62466e8cc56e38a283d29080952c895b00f59202
dimayu <dimayu82@gmail.com>
2021-06-13 23:02:08 +0300

    buil

---
+++ dist/js/common.js
@@ -0,0 +1,14 @@
+//*************MODAL*************** */
+
+$('.open-modal').click(function () {
+      var buttonId = $(this).attr('id');
+      $('#modal-container').removeAttr('class').addClass(buttonId);
+      $('body').addClass('modal-active');
+})
+
+$('.close').click(function () {
+      $('#modal-container').addClass('out');
+      $('body').removeClass('modal-active');
+});
+
+//****************MODAL END******************* */
